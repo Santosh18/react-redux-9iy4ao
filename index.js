910070a1c9f1d39468106fd385d39ea4c4d524cb
Santosh18 <santosh.swami93@gmail.com>
2019-08-17 23:39:22 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
 const store = createStore(reducer);
 
-render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
-
-
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
