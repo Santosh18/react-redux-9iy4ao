c2ce7ccb79409279fae1c88e7225ad786fb182f8
Santosh18 <santosh.swami93@gmail.com>
2019-08-18 01:00:56 +0530

    1 file modified

--- containers/Counter/Counter.js
+++ containers/Counter/Counter.js
@@ -11,10 +11,10 @@ class Counter extends Component {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add by 5" clicked={this.props.addValue}  />
-                <CounterControl label="Subtract by 5" clicked={this.props.subValue}  />
+                <CounterControl label="Increment : " clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement : " clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add by 5 : " clicked={this.props.addValue}  />
+                <CounterControl label="Subtract by 5 : " clicked={this.props.subValue}  />
             </div>
         );
     }
